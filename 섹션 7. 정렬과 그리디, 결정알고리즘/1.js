ad7bab2d4ba7fe50747c32c47f33e398c6f10cfb
Jaehyeon <parkjh9370@naver.com>
2022-02-10 09:46:52 +0900

    선택정렬 review

--- 섹션 7. 정렬과 그리디, 결정알고리즘/1.js
+++ 섹션 7. 정렬과 그리디, 결정알고리즘/1.js
@@ -1,12 +1,18 @@
+
 function solution(arr){
-    let answer = [...arr];
+    let answer = arr;
     for ( let i = 0; i< answer.length - 1; i++ ) {
+        // idx에 i번째를 저장
         let idx=i;
             for (let j=i+1; j < answer.length; j++ ) {
-                if (answer[j] < answer[idx]) idx=j;
-            } 
+                // answer[idx]의 수보다 작은 수가 발견되면 idx를 j로 바꿔줌
+                if (answer[j] < answer[idx]) {
+                    idx=j;
+                }
+            }
+            // 배열 중 가장 작은 인덱스 번호가 idx에 저장되어 있음
+        // 숫자 바꿔줌    
         [answer[i], answer[idx]] = [answer[idx], answer[i]];
-        console.log(answer)
     }
     return answer;
 }
