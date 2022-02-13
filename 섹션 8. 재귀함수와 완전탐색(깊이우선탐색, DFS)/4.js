fc6c12f0047eb2121cbb7451b4eeac0338b298bf
Jaehyeon <parkjh9370@naver.com>
2022-02-13 18:39:14 +0900

    부분집합 구하기(이진트리 DFS)

---
+++ 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/4.js
@@ -0,0 +1,25 @@
+function solution(n) {
+    let answer= [];;
+    let ch = Array.from({length:n+1}, ()=>0);
+    // console.log(ch)
+    DFS = (v) => {
+        if (v === n+1) {
+            let tmp = ""
+            for ( let i = 0; i <= n; i++) {
+                if (ch[i] === 1) tmp += i+ " ";
+            }
+            if (tmp.length > 0) answer.push(tmp.trim());
+        }
+        else {
+            ch[v] = 1;
+            DFS(v+1);
+            ch[v] = 0;
+            DFS(v+1);
+        }
+    }
+
+    DFS(1)
+    return answer
+}
+
+console.log(solution(3));
