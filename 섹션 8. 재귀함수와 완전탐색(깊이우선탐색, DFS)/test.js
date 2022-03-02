a1eaef8ca4cc832b5aa223680390faf517c448bc
Jaehyeon <parkjh9370@naver.com>
2022-03-02 20:32:20 +0900

    팩토리얼

--- 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/test.js
+++ 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/test.js
@@ -1,31 +1,13 @@
-// 순열: 중복을 허락하지 않음 -> 체크배열 필요
-
-function solution(m, arr) {
-  let answer = [];
-  n = arr.length;
-
-  let ch = Array.from({length: n}, ()=> 0);
-  let tmp = Array.from({length: m}, () => 0);
-
-  function DFS(L) {
-    if ( L === m ) {
-      answer.push(tmp.slice());
-    } 
-    else {
-      for ( let i = 0; i < n; i++ ) {
-        if ( ch[i] === 0 ) {
-          ch[i] = 1;
-          tmp[L]=arr[i];
-          DFS(L+1);
-          ch[i] = 0;
-        }
-      }
-    }
+function solution(n) {
+  let answer = 1;
+  
+  function DFS(n) {
+    if ( n === 1 ) return 1;
+    else return n * DFS(n-1);
   }
 
-  DFS(0);
+  answer = DFS(n)
   return answer;
 }
 
-let arr = [3, 6, 9];
-console.log(solution(2, arr))
+console.log(solution(5));
