8d9c053bb9d8a5b3cf58c83f0b4ec9be7b02c739
Jaehyeon <parkjh9370@naver.com>
2022-03-03 10:18:00 +0900

    조합수(메모이제이션

--- 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/test.js
+++ 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/test.js
@@ -1,21 +1,25 @@
-function solution(n,m) {
-  let answer = [];
-  let tmp = Array.from({length:m},()=>0)
-
-  function DFS(L) {
-    if (L===m) {
-      answer.push([...tmp])
-    }
-    else {
-      for (let i = 1; i <= n; i++ ) {
-        tmp[L] = i;
-        DFS(L+1);
-      }
-    }
+function solution(n, r) {
+  let answer; 
+  // 35행 35열의 2차원 배열
+  let dy = Array.from(Array(35), ()=> Array(35).fill(0))
+  
+  function DFS(n, r) {
+    if (dy[n][r] > 0) return dy[n][r]
+    if ( n === r || r === 0 ) return 1;
+    // 최종적으로 두개 더한 값이 리턴받는 값 더하기
+    else return dy[n][r] = DFS(n-1, r-1) + DFS(n-1,r)
   }
- 
-  DFS(0);
+
+  // 메모이제이션 : 재귀를 뻗으며 한 번 얻은 값들을 메모(기억) 해놓고
+  // 같은 수가 나오면 그걸 가져다 씀
+
+  answer = DFS(n, r)
   return answer;
 }
 
-console.log(solution(3,2));
+console.log(solution(5,3));
+console.log(solution(33,19));
+
+/**
+ * 5C3 = 4C2(내가 뽑혔다고 가정하고 조합) + 4C3(한개가 뽑히고 나머지에서 조합)
+ */
