41b924861641d9d048a79b65cfd2c97cab388999
Jaehyeon <parkjh9370@naver.com>
2022-03-16 08:58:29 +0900

    합이 같은 부분집합

--- 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/test.js
+++ 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/test.js
@@ -1,26 +1,17 @@
-function solution(c, arr) {
-  // 최댓값 구하는 경우 작은 수로 초기화
-  let answer = Number.MIN_SAFE_INTEGER;
-  let n = arr.length;
-
-  function DFS(L, sum) {
-    if (sum > c) return;
-    if (L === n) {
-      // console.log(sum)
-      answer = Math.max(answer, sum);
-    }
+function solution(n) {
+  let answer = "";
+  
+  DFS = (n) => {
+    if ( n===0 ) return
     else {
-      // 태운 경우
-      DFS(L+1, sum+arr[L]);
-      // 태우지 않은 경우 
-      DFS(L+1, sum);
+      DFS(parseInt(n/2))
+      answer += (n%2);
     }
   }
 
-  DFS(0,0);
-  return answer;
+  DFS(n)
+  return answer
 }
 
-
-let arr = [81, 58, 42, 33, 61];
-console.log(solution(259,arr));
+let output = solution(11);
+console.log(output);
