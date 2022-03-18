8a5c0ed2f53a01a2e0610e46733fe8d0485f7619
Jaehyeon <parkjh9370@naver.com>
2022-03-18 09:00:58 +0900

    팩토리얼

--- 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/11.js
+++ 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/11.js
@@ -4,9 +4,16 @@ function solution(n) {
     function DFS(n) {
       if ( n === 1 ) return 1;
       else return n * DFS(n-1);
+      // 5 * DFS(4)
+      // 5 * 4 * DFS(3)
+      // 5 * 4 * 3 * DFS(2)
+      // 5 * 4 * 3 * 2 * DFS(1)
+      // 5 * 4 * 3 * 2 * 1
     }
-  
+    
+    // 최종 값을 answer 변수에 담음
     answer = DFS(n)
+
     return answer;
   }
   
