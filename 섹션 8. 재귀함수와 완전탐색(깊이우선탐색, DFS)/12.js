b76a227b79321babba0f45969a52669cae991430
Jaehyeon <parkjh9370@naver.com>
2022-03-18 09:44:07 +0900

    조합수(메모이제이션)~

--- 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/12.js
+++ 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/12.js
@@ -1,25 +1,41 @@
+/**
+ * 조합수(메모이제이션)
+ * - 프로그래밍에서 반복되는 결과를 메모리에 저장 해놓고 다음에 같은 
+ * 결과가 나올 때 다시 계산할 필요없이 빨리 실행 하는 기법이다
+ */
+
+// Memoization
 function solution(n, r) {
-    let answer; 
-    // 35행 35열의 2차원 배열
-    let dy = Array.from(Array(35), ()=> Array(35).fill(0))
-    
-    function DFS(n, r) {
-      if (dy[n][r] > 0) return dy[n][r]
-      if ( n === r || r === 0 ) return 1;
-      // 최종적으로 두개 더한 값이 리턴받는 값 더하기
-      else return dy[n][r] = DFS(n-1, r-1) + DFS(n-1,r)
-    }
-  
-    // 메모이제이션 : 재귀를 뻗으며 한 번 얻은 값들을 메모(기억) 해놓고
-    // 같은 수가 나오면 그걸 가져다 씀
-  
-    answer = DFS(n, r)
-    return answer;
+  let answer;
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
+  // 35행 35열의 배열 만들어짐
+  // 문제에서 주어진 n, r 의 범위에 따라 설정 
+
+  function DFS(n, r) {
+    // 기록이 되었다면 초기화한 0보다 더 큰 수가 저장
+    if (dy[n][r] > 0) return dy[n][r]
+    // console.log(dy)
+    if ( n === r || r === 0 ) return 1
+    // dy배열에 리턴받는 값을 기록
+    else return dy[n][r] = DFS(n-1, r-1) + DFS(n-1, r);
   }
-  
-  console.log(solution(5,3));
-  console.log(solution(33,19));
-  
-  /**
-   * 5C3 = 4C2(내가 뽑혔다고 가정하고 조합) + 4C3(한개가 뽑히고 나머지에서 조합)
-   */
+ 
+  answer = DFS(n, r)
+  return answer;
+}
+
+console.log(solution(10, 3));
+
+// native solution
+// function solution(n, r) {
+//   let answer;
+
+//   function DFS(n, r) {
+//     if ( n === r || r === 0 ) return 1
+//     else return DFS(n-1, r-1) + DFS(n-1, r);
+//   }
+
+//   answer = DFS(n, r)
+//   return answer;
+// }
+
--- 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/test.js
+++ 섹션 8. 재귀함수와 완전탐색(깊이우선탐색, DFS)/test.js
@@ -1,17 +1,41 @@
-function solution(n) {
-  let answer = "";
-  
-  DFS = (n) => {
-    if ( n===0 ) return
-    else {
-      DFS(parseInt(n/2))
-      answer += (n%2);
-    }
-  }
+/**
+ * 조합수(메모이제이션)
+ * - 프로그래밍에서 반복되는 결과를 메모리에 저장 해놓고 다음에 같은 
+ * 결과가 나올 때 다시 계산할 필요없이 빨리 실행 하는 기법이다
+ */
 
-  DFS(n)
-  return answer
+// Memoization
+function solution(n, r) {
+  let answer;
+  let dy = Array.from(Array(35), () => Array(35).fill(0));
+  // 35행 35열의 배열 만들어짐
+  // 문제에서 주어진 n, r 의 범위에 따라 설정 
+
+  function DFS(n, r) {
+    // 기록이 되었다면 초기화한 0보다 더 큰 수가 저장
+    if (dy[n][r] > 0) return dy[n][r]
+    // console.log(dy)
+    if ( n === r || r === 0 ) return 1
+    // dy배열에 리턴받는 값을 기록
+    else return dy[n][r] = DFS(n-1, r-1) + DFS(n-1, r);
+  }
+ 
+  answer = DFS(n, r)
+  return answer;
 }
 
-let output = solution(11);
-console.log(output);
+console.log(solution(10, 3));
+
+// native solution
+// function solution(n, r) {
+//   let answer;
+
+//   function DFS(n, r) {
+//     if ( n === r || r === 0 ) return 1
+//     else return DFS(n-1, r-1) + DFS(n-1, r);
+//   }
+
+//   answer = DFS(n, r)
+//   return answer;
+// }
+
