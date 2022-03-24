bb18e06b7c5751e278d0cc2bec9e6de6f9ea5767
Jaehyeon <parkjh9370@naver.com>
2022-03-24 10:26:11 +0900

    거품정렬

--- 섹션 7. 정렬과 그리디, 결정알고리즘/2.js
+++ 섹션 7. 정렬과 그리디, 결정알고리즘/2.js
@@ -1,5 +1,26 @@
-function solution(arr){
-
-}
-
-let arr=[13, 5, 11, 7, 23, 15];
+function solution(arr) {
+    let answer = arr;
+  
+    for ( let i = 0; i < arr.length - 1; i++ ) {
+      for ( let j = 0; j < arr.length - i - 1; j++) {
+        if (arr[j] > arr[j+1]) {
+          [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+        }
+      }
+    }
+  
+    return answer;
+  }
+  
+  let arr = [13, 5, 11, 7, 23, 15];
+  console.log(solution(arr));
+  
+  /**
+    * 거품정렬(Bubble Sort)
+  1. 첫번째 for문을 통해 모든 배열 마지막 숫자 전까지를 탐색한다.
+  2. 두번째 for문을 통해 i값이 증가함에 따라 i값보다 한자리 앞의 숫자까지 탐색한다.
+  3. 만약 해당 숫자가 다음 숫자보다 클 경우 위치를 변경시켜 준다.
+  4. 첫번째 i = 0 일 경우를 모두 수행하고 나면 배열 중 가장 큰 수가 맨 뒤로 정렬 된다.
+  5. 두번째 i = 1일 경우를 모두 수행하면 나면 배열 중 두번째 큰 수가 맨 뒤 앞에 정렬된다.
+  6. 이를 반복한다.
+   */
--- 섹션 7. 정렬과 그리디, 결정알고리즘/test.js
+++ 섹션 7. 정렬과 그리디, 결정알고리즘/test.js
@@ -2,18 +2,25 @@ function solution(arr) {
   let answer = arr;
 
   for ( let i = 0; i < arr.length - 1; i++ ) {
-    // 초기 인덱스 번호 idx에 저장
-    let idx = i;
-    for ( let j = i+1; j < arr.length; j++ ) {
-      // arr[idx] 보다 작은 값 나타나면 그 인덱스를 idx로 바꿔줌
-      if (arr[j] < arr[idx]) idx = j;
+    for ( let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+      }
     }
-    // 각 배열을 교환
-    [arr[i], arr[idx]] = [arr[idx], arr[i]];
   }
 
   return answer;
 }
 
 let arr = [13, 5, 11, 7, 23, 15];
-console.log(solution(arr))
+console.log(solution(arr));
+
+/**
+  * 거품정렬(Bubble Sort)
+1. 첫번째 for문을 통해 모든 배열 마지막 숫자 전까지를 탐색한다.
+2. 두번째 for문을 통해 i값이 증가함에 따라 i값보다 한자리 앞의 숫자까지 탐색한다.
+3. 만약 해당 숫자가 다음 숫자보다 클 경우 위치를 변경시켜 준다.
+4. 첫번째 i = 0 일 경우를 모두 수행하고 나면 배열 중 가장 큰 수가 맨 뒤로 정렬 된다.
+5. 두번째 i = 1일 경우를 모두 수행하면 나면 배열 중 두번째 큰 수가 맨 뒤 앞에 정렬된다.
+6. 이를 반복한다.
+ */
