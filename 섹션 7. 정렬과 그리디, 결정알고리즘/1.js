d7ff570e0cff4f36df6eaecdc48c70f7a172be97
Jaehyeon <parkjh9370@naver.com>
2022-03-24 10:06:43 +0900

    선택정렬

--- 섹션 7. 정렬과 그리디, 결정알고리즘/1.js
+++ 섹션 7. 정렬과 그리디, 결정알고리즘/1.js
@@ -17,3 +17,12 @@ function solution(arr) {
   
   let arr = [13, 5, 11, 7, 23, 15];
   console.log(solution(arr))
+/**
+ * 선택 정렬(Selection Sort)
+1. 첫번째 for문을 통해 초기 인덱스 번호를 idx에 저장
+2. 두번째 for문을 통해 그 다음 인덱스를 확인하며 idx에 저장된 수보다 작은 경우 해당 idx 번호를 idx에 저장
+  (idx에는 가장 작은 수의 인덱스 번호가 저장됨)
+3. 저장된 idx 숫자와 초기 for문에서 설정된 idx 숫자와 자리를 바꿔준다.
+4. 해당 과정을 반복할 시 오름차순으로 숫자가 정렬된다.
+ */
+  
