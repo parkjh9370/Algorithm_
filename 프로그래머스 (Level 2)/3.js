dcbde5c891b54da355a61bfab254211656ba7df9
Jaehyeon <parkjh9370@naver.com>
2022-07-03 11:46:44 +0900

    큰수만들기

--- 프로그래머스 (Level 2)/3.js
+++ 프로그래머스 (Level 2)/3.js
@@ -1,21 +1,19 @@
 // 큰수만들기
 function solution(number, k) {
-    const stack = [];
+  const stack = [];
 
-    for ( let i = 0; i < number.length; i++ ) {
-        const el = number[i];
-        while ( k > 0 && stack[stack.length - 1] < el ) {
-            stack.pop();
-            k--;
-        }
-        // 동일한 수가 연속되었을 때 모든 수가 푸쉬되는 경우를 제외하기 위해
-        // number.length에서 k만큼 제거 된 숫자가 스택안에 존재하는 경우는
-        // 더 이상 푸쉬 하지 않는다.
-        if (stack.length < number.length - k ) stack.push(el);
+  for (let i = 0; i < number.length; i++) {
+    const el = number[i];
+    while (k > 0 && stack[stack.length - 1] < el) {
+      stack.pop();
+      k--;
     }
+    // 동일한 수가 연속되었을 때 모든 수가 푸쉬되는 경우를 제외하기 위해
+    // number.length에서 k만큼 제거 된 숫자가 스택안에 존재하는 경우는
+    // 더 이상 푸쉬 하지 않고 while문 종료
+    if (stack.length < number.length - k) stack.push(el);
+  }
 
-    stack.splice(stack.length - k, k)
-    return stack.join('');
-
+  stack.splice(stack.length - k, k);
+  return stack.join("");
 }
-
