8c944a6b029ad578498044860f7775e6e290c984
Jaehyeon <parkjh9370@naver.com>
2022-07-12 11:17:22 +0900

    6.1 stack

--- 섹션 6. 자료구조(스택, 큐)/1.js
+++ 섹션 6. 자료구조(스택, 큐)/1.js
@@ -1,34 +1,32 @@
 /**
  * 스택 - LIFO(Last In First Out)
- * -> 나중에 들어간 것이 가장 먼저 나옴
- *  
+ * -> 나중에 들어간 것이 가장 먼저 나!옴
+ *
  * 큐 - FIFO(First In First Out)
- * -> 먼저 들어간 것이 가장 먼저 나옴 
+ * -> 먼저 들어간 것이 가장 먼저 나옴!
  */
 
 function solution(s) {
-    let answer = "YES";
+  let answer = "YES";
 
-    // '(' 기호면 stack에 push
-    // ')' 기호면 stack에서 pop
-    let stack = [];
+  // '(' 기호면 stack에 push
+  // ')' 기호면 stack에서 pop
+  let stack = [];
 
-    // ')' 기호가 더 많은 case 
-    for (let index of s) {
-        if ( index === '(') stack.push(index);
-        else {
-            if (stack.length === 0 ) return "NO"
-            stack.pop();
-        }
+  // ')' 기호가 더 많은 case
+  for (let index of s) {
+    if (index === "(") stack.push(index);
+    else {
+      if (stack.length === 0) return "NO";
+      stack.pop();
     }
+  }
 
-    // '(' 기호가 더 많은 경우 stack에 아직 data가 남아 있음
-    if (stack.length > 0) return "NO"
+  // '(' 기호가 더 많은 경우 stack에 아직 data가 남아 있음
+  if (stack.length > 0) return "NO";
 
-    return answer
+  return answer;
 }
-
-
 
 let a = "(()(()))(()";
 console.log(solution(a));
